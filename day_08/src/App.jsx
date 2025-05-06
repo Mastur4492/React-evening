2bc2d1bf6ae1a2517f4ff9c6674e6496da57be76
Mastur <bikanerwalamastur13@gmail.com>
2025-05-06 19:16:06 +0530

    new file addded

--- day_08/src/App.jsx
+++ day_08/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import FormMultipul from './components/FormMultipul'
+// import FormMultipul from './components/FormMultipul'
+import FormUsingFormHook from './components/FormUsingFormHook'
 // import FormApp from './components/FormApp'
 
 const App = () => {
   return (
     <div>
       {/* <FormApp/> */}
-      <FormMultipul/>
+      {/* <FormMultipul/> */}
+      <FormUsingFormHook/>
     </div>
   )
 }
